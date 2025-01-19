5a9d17adf65aed339a088020062d9f1dda5358d7
omborate100 <omcode100@gmail.com>
2025-01-19 19:09:40 +0530

    nested structure of html tag using react

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-const element = React.createElement('h1', {className: 'greeting'}, 'Hello, world from React! 🚀🚀');
-
-console.log(element); // Its an object
-
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>Hello 1, world from React! 🚀</h1> 
+ *          <h1>Hello 2, world from React! 🚀</h1>   
+   
+ *      </div>
+ * </div>
+ * 
+ * ReactDom(object) => HTML       // render method is responsible to convert object to HTML
+ * 
+ */
+const parent = React.createElement(
+    'div', //tag
+     {id: 'parent'},   //attributes
+     React.createElement('div',{id:'child'},
+        [
+            React.createElement('h1', {id :"child"}, 'Hello 1, world from React! 🚀'), //content
+            React.createElement('h1', {id :"child"}, 'Hello 2, world from React! 🚀') //content
+        ]
+     ));
+console.log(parent);
 const root = document.getElementById('root');
-ReactDOM.render(element, root);
+ReactDOM.render(parent, root); 
